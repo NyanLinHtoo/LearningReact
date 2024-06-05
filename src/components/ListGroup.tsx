58775ff2c377bd31fbc7ead8c9712464e0c1576e
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-05 13:32:14 +0700

    update simple hook

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,29 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const items = ["Yangon", "Mandalay", "Myanmar", "Bangkok"];
-
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
 
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 ? <p>No Item Found</p> : null}
       <ul className="list-inside list-disc">
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
-            <li key={item} onClick={handleClick}>
+            <li
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+              className={
+                selectedIndex === index
+                  ? " bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  : "list-inside list-disc"
+              }>
               {item}
             </li>
           );
