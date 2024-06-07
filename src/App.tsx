fb532d5b7f16a9c00af0006c97871287254e2f4e
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-07 15:47:22 +0700

    Update nested Object

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
     title: "Americano",
     price: 5,
   });
+  const [person, setPerson] = useState({
+    name: "John",
+    age: 30,
+    address: {
+      street: "Main St",
+      city: "New York",
+    },
+  });
 
   const handleClick = () => {
     const newDrint = {
@@ -13,10 +21,22 @@ function App() {
       price: (drink.price += 1),
     };
     setDrink(newDrint);
+
+    const newData = {
+      ...person,
+      age: person.age + 1,
+      address: {
+        ...person.address,
+        city: "Yangon",
+      },
+    };
+    setPerson(newData);
   };
   return (
     <div className="p-8">
-      {drink.title} {drink.price}
+      {drink.title} {drink.price} <br />
+      {person.name}
+      {person.age} {person.address.city}
       <Like onClick={handleClick} />
     </div>
   );
