70b6e6a15d5f32390471f5ef2038e321456d6b3b
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-08 01:40:53 +0700

    react form with tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import Expandable from "./components/Expandable";
-
 function App() {
   return (
-    <div className="p-8">
-      <Expandable maxChars={20}>Hello World</Expandable>
-    </div>
+    <form className="max-w-sm p-5">
+      <div className="mb-3">
+        <label htmlFor="name" className="block mb-2 text-sm font-medium ">
+          Your Name
+        </label>
+        <input
+          type="name"
+          id="name"
+          className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="age" className="block mb-2 text-sm font-medium ">
+          Your Age
+        </label>
+        <input
+          type="number"
+          id="number"
+          className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:text-white "
+          required
+        />
+      </div>
+      <button className="text-white rounded bg-blacktext-white bg-blue-700 hover:bg-blue-800 px-5 py-2.5">
+        Submit
+      </button>
+    </form>
   );
 }
 
