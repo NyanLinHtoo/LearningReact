5e25a11318990e798731eabad06fe4af8463e574
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-08 03:33:54 +0700

    Applying Validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { FieldValues, useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  interface formData {
+    name: string;
+    age: number;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formData>();
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -14,13 +23,19 @@ function App() {
           Your Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           type="name"
           id="name"
           className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-          placeholder="name@flowbite.com"
-          required
         />
+        {errors.name?.type === "required" && (
+          <span className="text-sm text-red-600">Name is required</span>
+        )}
+        {errors.name?.type === "minLength" && (
+          <span className="text-sm text-red-600">
+            Name Character must be over 3.
+          </span>
+        )}
       </div>
       <div className="mb-5">
         <label htmlFor="age" className="block mb-2 text-sm font-medium ">
@@ -31,7 +46,6 @@ function App() {
           type="number"
           id="number"
           className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
-          required
         />
       </div>
       <button className="text-white rounded bg-blacktext-white bg-blue-700 hover:bg-blue-800 px-5 py-2.5">
