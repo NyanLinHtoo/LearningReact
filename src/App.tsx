7d3633ae6d42157d2be7b77f8d109facd91e1f1f
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-17 18:10:41 +0700

    Learning useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,24 @@
 import { useState } from "react";
-import ExpenseForm from "./components/Expense/ExpenseForm";
-import ExpenseList from "./components/Expense/ExpenseList";
-import ExpenseFilter from "./components/Expense/ExpenseFilter";
+import ProductList from "./components/ProductList";
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "Milk",
-      amount: 5,
-      category: "Groceries",
-    },
-    {
-      id: 2,
-      description: "Eggs",
-      amount: 10,
-      category: "Groceries",
-    },
-    {
-      id: 3,
-      description: "Electricity",
-      amount: 100,
-      category: "Utilities",
-    },
-    {
-      id: 4,
-      description: "Movies",
-      amount: 15,
-      category: "Entertainment",
-    },
-  ]);
-
-  const visiableCategory = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
-  const onDelete = (id: number) => {
-    setExpenses(expenses.filter((expense) => expense.id !== id));
-  };
+const App = () => {
+  const [category, setCategory] = useState("");
 
   return (
-    <div>
-      <ExpenseForm
-        onSubmit={(expense) =>
-          setExpenses([
-            ...expenses,
-            {
-              ...expense,
-              id: expenses.length + 1,
-            },
-          ])
-        }
-      />
-      <ExpenseFilter
-        onSelectCategory={(category) => setSelectedCategory(category)}
-      />
-      <ExpenseList expenses={visiableCategory} onDelete={onDelete} />
+    <div className="max-w-md mx-auto p-3">
+      <select
+        className="border border-black "
+        onChange={(event) => {
+          console.log(event.target.value);
+          setCategory(event.target.value);
+        }}>
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="HouseHold">HouseHold</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
-}
+};
 
 export default App;
