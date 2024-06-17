b8ff6bb0f4944d31be46db9302d0fc9c6a3890c9
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-18 02:17:33 +0700

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
+import Loader from "./components/Loader/Loader";
 
 interface User {
   id: number;
@@ -9,30 +10,43 @@ interface User {
 const App = () => {
   const [users, SetUsers] = useState<User[]>([]);
   const [errors, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     const fetchData = async () => {
       try {
         const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/xusers"
+          "https://jsonplaceholder.typicode.com/users"
         );
         SetUsers(res.data);
+        setErrors("");
+        setLoading(false);
       } catch (err) {
         setErrors((err as AxiosError).message);
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
 
   return (
-    <>
+    <div className="max-w-md mx-auto mt-44">
       {errors && <p className="text-red-600 font-medium">{errors}</p>}
-      <ul>
+      {isLoading && (
+        <div className="mx-auto pl-10">
+          <Loader />
+        </div>
+      )}
+      <ul className="list-inside ">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="list-disc">
+            {user.name}
+          </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
